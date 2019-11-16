f63782bc9932948c55b3d6a41de9c424abfebeba
Dave Ward <professorx00@gmail.com>
2019-11-16 18:39:55 -0500

    Try url edit

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreat
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.get('/callback', function (req, res) {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // enhance your app security with Helmet
