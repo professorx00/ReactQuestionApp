de3c62b8341380f61afe0a9ecb1aefe59aaab000
Dave Ward <professorx00@gmail.com>
2019-11-16 19:39:31 -0500

    Test27--removed Buid from gitignore

---
+++ frontend/build/precache-manifest.0355d12a8b5c82280d0c0fd8ddf3a577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4dda2ad133a6ead64d6af021dea9523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0759e6d3dbc0fc50d22",
+    "url": "/static/css/main.921abc0e.chunk.css"
+  },
+  {
+    "revision": "da205de8ef7cf95049f2",
+    "url": "/static/js/2.ca46db6d.chunk.js"
+  },
+  {
+    "revision": "e0759e6d3dbc0fc50d22",
+    "url": "/static/js/main.36255cdf.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
